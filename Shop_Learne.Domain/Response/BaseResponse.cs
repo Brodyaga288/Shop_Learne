c77f7db8b0b635b84c54f1b918bab6b4f683713d
Brodyaga <igorverhovin391@gmail.com>
2024-03-13 19:11:50 +0000

    AddResponse

---
+++ Shop_Learne.Domain/Response/BaseResponse.cs
@@ -0,0 +1,16 @@
+﻿using Shop_Learne.Domain.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop_Learne.Domain.Response
+{
+    public class BaseResponse<T>
+    {
+        public string Descroption { get; set; }
+        public StatusCode StatusCode{ get; set; }
+        public T Data{ get; set; }
+    }
+}
